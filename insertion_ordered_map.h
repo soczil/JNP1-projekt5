f846cae96ab2f807038b3deac9984e7843de66d1
adamrempala <rempalaad@gmail.com>
2019-12-19 10:15:01 +0100

    Dodanie seta referenced – trzyma wartości, dla których była robiona referencja. W przypadku, gdy set niepusty, nie możemy robić copy-on-write.

--- insertion_ordered_map.h
+++ insertion_ordered_map.h
@@ -5,6 +5,7 @@
 #include <list>
 #include <utility>
 #include <unordered_map>
+#include <unordered_set>
 #include <memory>
 
 using namespace std;
@@ -20,12 +21,76 @@ class insertion_ordered_map {
 private:
     shared_ptr<list<pair<K, V>>> values_with_keys;
     shared_ptr<unordered_map<K, typename list<pair<K, V>>::iterator>> map_ptr;
+    shared_ptr<unordered_set<K>> referenced; // if there are elements, we cannot copy-on-write
+    void copy(insertion_ordered_map &copied) {
+        // here we have to add copying
+        referenced = make_shared<unordered_set<K>>();
+    }
 
 public:
     insertion_ordered_map() {
         values_with_keys = make_shared<list<pair<K, V>>>();
         map_ptr = make_shared<unordered_map<K, typename list<pair<K, V>>::iterator>>();
+        referenced = make_shared<unordered_set<K>>();
     }
+
+    insertion_ordered_map(insertion_ordered_map const &other) {
+        if (!referenced->size()) {
+            map_ptr = other.map_ptr;
+            values_with_keys = other.values_with_keys;
+            referenced = make_shared<unordered_set<K>>();
+        }
+        else {
+            copy(other);
+        }
+    }
+
+    insertion_ordered_map(insertion_ordered_map &&other) = default;
+
+    insertion_ordered_map &operator=(insertion_ordered_map other) = default;
+
+    bool insert(K const &k, V const &v) {
+
+    }
+
+    void erase(K const &k) {
+        referenced->erase(k);
+    }
+
+    void merge(insertion_ordered_map const &other) {
+
+    }
+
+    V &at(K const &k) {
+        referenced->insert(k);
+    }
+
+    V const &at(K const &k) const {
+
+    }
+
+    V &operator[](K const &k) {
+
+    }
+
+    size_t size() const {
+        return map_ptr->size();
+    }
+
+    bool empty() const {
+        return size() == 0;
+    }
+
+    void clear() {
+        values_with_keys = make_shared<list<pair<K, V>>>();
+        map_ptr = make_shared<unordered_map<K, typename list<pair<K, V>>::iterator>>();
+        referenced = make_shared<unordered_set<K>>();
+    }
+
+    bool contains(K const &k) const {
+        return map_ptr->find(k) != (*map_ptr).end();
+    }
+
 
 };
 
